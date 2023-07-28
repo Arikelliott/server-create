91fa967c3aec0a51b0afa40677d77f8cc1356ec7
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2023-07-28 12:30:00 -0400

    Deleted extraneous files

--- scripting-test-2.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-myvar="foo/bar/baz"
-pattern_var="pattern"
-pattern="s/bar/123/"
-
-result=$(echo "$myvar" | sed "${!pattern_var}")
-echo "$result"
--- scripting-test-3.sh
+++
@@ -1,6 +0,0 @@
-# #! /bin/bash
-
-sitekey=fred
-toplevel_domain=.com
-bob=$sitekey\\$toplevel_domain
-echo "$bob/the/builder"
